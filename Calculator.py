f22e8283c4ab87263c4cb94d370431f74aae04be
rsevial <rebekahsevial@gmail.com>
2023-05-21 23:23:01 +0800

    Unfinished pseudocode for Calculator file

--- Calculator.py
+++ Calculator.py
@@ -4,3 +4,8 @@
 # Import the Calculator class from Class Calculator file
 # Import the UserInterface class from User Interface file
 # Import the Error class of Class fromErrors file
+# Create object for the three file
+# Start
+# Display the greetings of Calculator
+# Display the four operators of Calculator
+# 
