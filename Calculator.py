f3ea384414b157d46973d37675af54537656f201
rsevial <rebekahsevial@gmail.com>
2023-05-21 23:29:37 +0800

    Import all of the classes to Calculator from three files

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,12 @@
 # Programmed by: Rebekah Joy E. Sevial
 # Creating App Calculator using OOP
 
-# Import the Calculator class from Class Calculator file
-# Import the UserInterface class from User Interface file
-# Import the Error class of Class fromErrors file
+# Import the Calculator class from class_calculator file
+from class_calculator import Calculator
+# Import the UserInterface class from user_interface file
+from user_interface import UserInterface
+# Import the Error class of class_from_errors file
+from class_for_errors import Error
 # Create object for the three file
 # Start
 # Display the greetings of Calculator
