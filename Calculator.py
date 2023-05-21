7712b3300cea68d320d3c684ea0e087fc4c53dfc
rsevial <rebekahsevial@gmail.com>
2023-05-21 23:33:15 +0800

    Create object for the three class

--- Calculator.py
+++ Calculator.py
@@ -7,7 +7,11 @@ from class_calculator import Calculator
 from user_interface import UserInterface
 # Import the Error class of class_from_errors file
 from class_for_errors import Error
-# Create object for the three file
+# Create object for the three class
+calcu = Calculator()
+ui = UserInterface()
+error = Error()
+
 # Start
 # Display the greetings of Calculator
 # Display the four operators of Calculator
