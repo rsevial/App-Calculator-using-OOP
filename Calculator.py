28f9daae3caeaa7f0bd6e62220fb2650d882252a
rsevial <rebekahsevial@gmail.com>
2023-05-24 22:30:21 +0800

    Give comments for Calculator

--- Calculator.py
+++ Calculator.py
@@ -16,38 +16,41 @@ error = Error()
 ui.greet()
 # Display the four operators of Calculator
 ui.display_operations()
-# Asks the user to decide which operation to use assigned from numbers 1 to 4
+# Use While lopp and Asks the user to decide which operation to use assigned from numbers 1 to 4
 # Use the try and except function to check for the errors
 while True:
     try: 
         operation = ui.ask_operator()
     except(ValueError, TypeError):
         error.value_and_type_error()
-
+    # Add condition to check if the user inputted valid numbers
+    # If operation <= 4: ask user for num1 and num2
+    # Use try and except function if the user inputted invalid numbers
     if operation <= 4:
         try:
             num1 = ui.ask_number1()
             num2 = ui.ask_number2()
         except(ValueError, TypeError):
             error.value_and_type_error() 
-
+    # print sum if the user choose 1
     if operation == 1:
         sum = calcu.add(num1,num2)
         ui.print_sum(sum)
+    # print difference if the user choose 2
     elif operation == 2:
         difference = calcu.subtract(num1, num2)
         ui.print_difference(difference)
+    # print product if the user choose 3
     elif operation == 3:
         product = calcu.multiply(num1, num2)
         ui.print_product(product)
+    # print quotient if the user choose 4
     elif operation == 4:
         try:
             quotient = calcu.divide(num1, num2)
             ui.print_quotient(product)
+        # except if the num2 == 0: use except ZeroDivisionError
         except ZeroDivisionError:
             error.zero_division_error()
     else:
-        error.invalid_error()     
-# Add a condition when the user inputted an invalid number
-                
-# Call for the function display menu
+        error.invalid_error()                
