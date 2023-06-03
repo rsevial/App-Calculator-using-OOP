70583228141fbc1e1182547c4180512f6bad0b25
rsevial <rebekahsevial@gmail.com>
2023-06-03 12:10:28 +0800

    Create new class from the imported class

--- new_class.py
+++ new_class.py
@@ -1,8 +1,10 @@
 # Programmed by: Rebekah Joy E. Sevial
 # Create a new class that inherits from existing class in your calculator program
 
-# import a class from two files
+# import a class from files
 from user_interface import UserInterface
-# create class from the imported class
+# create new class from the imported class
+class NewUserInterface(UserInterface):
+    pass
 # add new methods 
 # override an existing methods
