7a5c90085966305a2c54eb25fae8ae4b9bd4b6e9
rsevial <rebekahsevial@gmail.com>
2023-06-03 12:00:12 +0800

    Create Pseudocode for the new class file

---
+++ new_class.py
@@ -0,0 +1,7 @@
+# Programmed by: Rebekah Joy E. Sevial
+# Create a new class that inherits from existing class in your calculator program
+
+# import a class from two files
+# create class from the imported class
+# add new methods 
+# override an existing methods
